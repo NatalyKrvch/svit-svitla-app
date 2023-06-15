37d6f5c72329ebe3855aa63eda79fb282c81e8fe
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2023-06-15 20:44:30 +0300

    making routes in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
+import ProductsCatalogue from "module";
+import Authorisation from "./pages/AuthorisationPage";
+import Authors from "./pages/AuthorsPage";
+import CreateCard from "./pages/CreateCardPage";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<ProductsCatalogue />} />
+        <Route path="/authorisation" element={<Authorisation />} />
+        <Route path="/authors" element={<Authors />} />
+        <Route path="/createcard" element={<CreateCard />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/CreateCardPage.jsx
@@ -0,0 +1,5 @@
+function CreateCard() {
+  return <>Create Card</>;
+}
+
+export default CreateCard;
