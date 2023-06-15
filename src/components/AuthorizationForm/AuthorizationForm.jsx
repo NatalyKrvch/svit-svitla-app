86cec56ee97ff6d1c90d41fcff6ef994dea531cf
KsenyaKoss <roxenn34@gmail.com>
2023-06-15 08:56:22 +0300

    ADDED: forms; new favicon, title; some pictures; reset styles

--- src/components/AuthorizationForm/AuthorizationForm.jsx
+++ src/components/AuthorizationForm/AuthorizationForm.jsx
@@ -3,10 +3,10 @@ const AuthorizationForm = () => {
         <>
         <h1>Авторизуйтесь</h1>
         <form action="">
-            <label htmlFor="">Логін</label>
-            <input type="text" name="login" placeholder="e.g.lampalampa"/>
-            <label htmlFor="">Пароль</label>
-            <input type="password" name="password" placeholder="все з маленької" />
+            <label htmlFor="login">Логін</label>
+            <input id="login" type="text"  placeholder="e.g.lampalampa"/>
+            <label htmlFor="password">Пароль</label>
+            <input type="password" id="password" placeholder="все з маленької" />
             <button type="submit">Увійти</button>
         </form>    
         </>
