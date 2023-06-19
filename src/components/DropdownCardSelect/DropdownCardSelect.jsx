81cb3f5a9aab81cb3937326d117e36e5b89a4b77
KsenyaKoss <roxenn34@gmail.com>
2023-06-19 22:58:28 +0300

    ADDED: DropdownSelect component

--- src/components/DropdownCardSelect/DropdownCardSelect.jsx
+++ src/components/DropdownCardSelect/DropdownCardSelect.jsx
@@ -1,58 +1,21 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import { StyledSelect, StyledWrpSelector } from "./SelectCardSelectorStyled";
 
-const ProductForm = () => {
-  const [productName, setProductName] = useState("");
-  const [productCode, setProductCode] = useState("");
-  const [price, setPrice] = useState("");
-  const [manufacturerCountry, setManufacturerCountry] = useState("");
-  const [coverImage, setCoverImage] = useState(null);
-  const [productImages, setProductImages] = useState([]);
+const SelectCardSelector = () => {
+  const [filter, setFilter] = useState("Вид картки");
 
-  const handleProductNameChange = (event) => {
-    setProductName(event.target.value);
-  };
-
-  const handleProductCodeChange = (event) => {
-    setProductCode(event.target.value);
-  };
-
-  const handlePriceChange = (event) => {
-    setPrice(event.target.value);
-  };
-
-  const handleManufacturerCountryChange = (event) => {
-    setManufacturerCountry(event.target.value);
-  };
-
-  const handleCoverImageChange = (event) => {
-    const file = event.target.files[0];
-    setCoverImage(file);
-  };
-
-  const handleProductImagesChange = (event) => {
-    const files = event.target.files;
-    setProductImages(Array.from(files));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // Добавьте здесь логику для обработки отправки формы
-    // Например, можно отправить данные на сервер или выполнить другие действия
-
-    // Сбросить значения полей формы после отправки
-    setProductName("");
-    setProductCode("");
-    setPrice("");
-    setManufacturerCountry("");
-    setCoverImage(null);
-    setProductImages([]);
-  };
+  const handleChangeOptionFilter = (evt) => setFilter(evt.target.value);
 
   return (
-      <>
-      </>
+    <>
+      <StyledWrpSelector>
+        <StyledSelect value={filter} onChange={handleChangeOptionFilter}>
+          <option value="productCard">Картка товару</option>
+          <option value="catalogCard">Картка каталогу</option>
+        </StyledSelect>
+      </StyledWrpSelector>
+    </>
   );
 };
 
-export default ProductForm;
+export default SelectCardSelector;
