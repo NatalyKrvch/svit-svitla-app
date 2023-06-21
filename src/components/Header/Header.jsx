9e70b4c8981dba15de8a0a2e36cd227b538a63f0
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2023-06-21 23:03:05 +0300

    logo & header adaptation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,46 @@
-//import { useMediaRules } from "../../hooks/useMediaRules";
-import { StyledHeader, HeaderWrapper, LogoWrapper } from "./HeaderStyled";
+import { useMediaRules } from "../../hooks/useMediaRules";
+import {
+  StyledHeader,
+  HeaderWrapper,
+  LogoWrapper,
+  Image,
+  BurgerWrapper,
+} from "./HeaderStyled";
 
 function Header() {
-  // const { isMobile, isDesktop, isTablet } = useMediaRules();
+  const { isMobile, isDesktop, isTablet } = useMediaRules();
   return (
     <>
       <StyledHeader>
         <HeaderWrapper>
-          <LogoWrapper />
-          {/* тут ще буде умовний рендеринг меню в залежності від розміру екрану */}
+          <LogoWrapper>
+            {isDesktop && (
+              <Image
+                src="src/images/Logo/Desktop/Header/logo_des@1x.svg"
+                alt="Logo"
+              />
+            )}
+            {isTablet && (
+              <Image
+                src="src/images/Logo/Tablet/Header/logo_tab@1x.svg"
+                alt="Logo"
+              />
+            )}
+            {isMobile && (
+              <Image
+                src="src/images/Logo/Mobile/Header/logo_mob@1x.svg"
+                alt="Logo"
+              />
+            )}
+          </LogoWrapper>
+          {isMobile && (
+            <>
+              <BurgerWrapper>
+                <Image src="src/images/Menu/Burger.svg" alt="Menu" />
+              </BurgerWrapper>
+            </>
+          )}
+          {/* тут ще буде умовний рендеринг меню для планшета і десктопа */}
         </HeaderWrapper>
       </StyledHeader>
     </>
--- src/components/Header/HeaderStyled.js
+++ src/components/Header/HeaderStyled.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
+
 export const StyledHeader = styled.header`
     background-color: var(--dark-blue);
     height: 56px;
     display: 'flex';
-    justify-content: center;
-    align-items: center;
+    justifyContent: 'space-between';
+    alignItems: 'center';
 
         @media screen and (min-width: 1280px) {
             height: 64px;
@@ -15,13 +16,22 @@ export const StyledHeader = styled.header`
 export const HeaderWrapper = styled.div`
     display: 'flex';
     justifyContent: 'space-between';
+    alignSelf: 'center';
     height: 56px;
-        
+ 
         @media screen and (min-width: 1280px) {
             height: 64px;
         }
 `;
 
 export const LogoWrapper = styled.div`
-        
-`
+    display: 'flex';
+    alignSelf: 'center';
+`;
+
+export const Image = styled.img`
+    
+    height: auto;
+`;
+
+export const BurgerWrapper = styled.div``
