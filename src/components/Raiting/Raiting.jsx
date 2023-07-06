0d1a90632651f82c7f62575a29adfcd2f689bdd1
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2023-07-06 11:49:54 +0300

    refactoring

--- src/components/Raiting/Raiting.jsx
+++ src/components/Raiting/Raiting.jsx
@@ -1,33 +1,15 @@
-import {
-  StyledH1,
-  PageWrapper,
-  StyledP,
-  StyledTextarea,
-  StyledButton,
-  StyledForm,
-  StarsContainer,
-} from "./RaitingStyled";
-import { useState, useEffect } from "react";
+import { StarsContainer } from "./RaitingStyled";
+import { useState } from "react";
 import Sprite from "../../images/symbol-defs.svg";
 import { useMediaRules } from "../../hooks/useMediaRules";
-// import { useDispatch } from "react-redux";
-// import { addReview } from "../../redux/Review/reviewOperations";
 
 function Raiting() {
   const [selectedStars, setSelectedStars] = useState([]);
-  const [feedback, setFeedback] = useState("");
 
   const { isTablet, isDesktop } = useMediaRules();
 
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   dispatch(addReview(reviewMark, feedback));
-  // }, [dispatch, reviewMark, feedback]);
-
   const starCounter = [1, 2, 3, 4, 5];
   let starSize = 36;
-
   if (isDesktop || isTablet) {
     starSize = 48;
   }
@@ -38,66 +20,29 @@ function Raiting() {
     setSelectedStars(selectedStars);
   };
 
-  const reviewMark = selectedStars[selectedStars.length - 1];
-
-  console.log(selectedStars);
-
-  const sendMail = () => {
-    var link =
-      "mailto:svitsvitlacn@ukr.net" +
-      "&subject=" +
-      encodeURIComponent("Feedback") +
-      "&body=" +
-      encodeURIComponent(document.getElementById("feedback").value);
-    window.location.href = link;
-    console.log("button is clicked");
-  };
-
-  const handleChange = (event) => {
-    setFeedback(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Додаткова логіка для відправки даних
-  };
+  // const reviewMark = selectedStars[selectedStars.length - 1];
 
   return (
     <>
-      <PageWrapper>
-        <StyledH1>Залиште відгук тут</StyledH1>
-        <StyledP>Як вам було у Світі світла?</StyledP>
-        <StarsContainer>
-          {starCounter.map((key) => {
-            const isSelected = selectedStars.includes(key);
-            return (
-              <svg
-                key={key}
-                width={starSize}
-                height={starSize}
-                onClick={() => handleStarClick(key)}
-                style={{ cursor: "pointer" }}
-              >
-                <use
-                  href={`${Sprite}#icon-star`}
-                  style={{ fill: isSelected ? "#FFE34E" : "#DBE4E8" }}
-                />
-              </svg>
-            );
-          })}
-        </StarsContainer>
-        <StyledForm onSubmit={handleSubmit}>
-          <StyledTextarea
-            onChange={handleChange}
-            type="text"
-            placeholder="Ваші враження"
-            id="feedback"
-          />
-          <StyledButton type="submit" onClick={sendMail}>
-            Надіслати
-          </StyledButton>
-        </StyledForm>
-      </PageWrapper>
+      <StarsContainer>
+        {starCounter.map((key) => {
+          const isSelected = selectedStars.includes(key);
+          return (
+            <svg
+              key={key}
+              width={starSize}
+              height={starSize}
+              onClick={() => handleStarClick(key)}
+              style={{ cursor: "pointer" }}
+            >
+              <use
+                href={`${Sprite}#icon-star`}
+                style={{ fill: isSelected ? "#FFE34E" : "#DBE4E8" }}
+              />
+            </svg>
+          );
+        })}
+      </StarsContainer>
     </>
   );
 }
--- src/pages/FeedbackPage/FeedbackPage.jsx
+++ src/pages/FeedbackPage/FeedbackPage.jsx
@@ -1,9 +1,52 @@
 import Raiting from "../../components/Raiting/Raiting";
+import {
+  StyledH1,
+  PageWrapper,
+  StyledP,
+  StyledTextarea,
+  StyledButton,
+  StyledForm,
+} from "./FeedbackPageStyled";
+import { useState, useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { addReview } from "../../redux/Review/reviewOperations";
 
 function Feedback() {
+  const [feedback, setFeedback] = useState("");
+
+  const handleChange = (event) => {
+    setFeedback(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Додаткова логіка для відправки даних
+  };
+
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch(addReview(reviewMark, feedback));
+  // }, [dispatch, reviewMark, feedback]);
+
   return (
     <>
-      <Raiting />
+      <PageWrapper>
+        <StyledH1>Залиште відгук тут</StyledH1>
+        <StyledP>Як вам було у Світі світла?</StyledP>
+        <Raiting />
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledTextarea
+            onChange={handleChange}
+            type="text"
+            placeholder="Ваші враження"
+            id="feedback"
+          />
+          <StyledButton type="submit" onSubmit={handleSubmit}>
+            Надіслати
+          </StyledButton>
+        </StyledForm>
+      </PageWrapper>
     </>
   );
 }
