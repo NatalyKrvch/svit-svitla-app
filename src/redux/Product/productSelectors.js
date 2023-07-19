006a0af835fbd1c41f74ecfc08d91f08c75d4abb
Loki99911 <nikita_kresik@i.ua>
2023-07-19 12:15:44 +0300

    Add Selector Total

--- src/redux/Product/productSelectors.js
+++ src/redux/Product/productSelectors.js
@@ -1,4 +1,5 @@
 export const getAllProducts = (state) => state.products.products;
 export const getLoadingProducts = (state) => state.products.isLoading;
 export const getErrorProducts = (state) => state.products.error;
-export const getCurrentProduct = state => state.products.currentProduct;
+export const getCurrentProduct = (state) => state.products.currentProduct;
+export const getTotalItems = (state) => state.products.totalItems;
