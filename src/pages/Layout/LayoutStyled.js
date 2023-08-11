1cc2f37c8d0693a27af448952cb2080ed4db3ef7
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2023-08-11 20:21:37 +0300

    нижній скрол

--- src/pages/Layout/LayoutStyled.js
+++ src/pages/Layout/LayoutStyled.js
@@ -9,16 +9,16 @@ export const Wrapper = styled.div`
 export const Main = styled.main`
   flex: 1;
   display: flex;
-  padding-top: 40px;
-  padding-bottom: 32px;
+  margin-top: 40px;
+  margin-bottom: 32px;
 
   @media screen and (min-width: 768px) {
-    padding-top: 64px;
-    padding-bottom: 48px;
+    margin-top: 64px;
+    margin-bottom: 48px;
   }
 
   @media screen and (min-width: 1280px) {
-    padding-top: 72px;
-    padding-bottom: 56px;
+    margin-top: 72px;
+    margin-bottom: 56px;
   }
 `;
