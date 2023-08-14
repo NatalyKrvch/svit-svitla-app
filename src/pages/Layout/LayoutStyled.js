f7320df875268ed8c14b63539de13f1c254e4697
Nataliia Kravchenko <105675637+NatalyKrvch@users.noreply.github.com>
2023-08-14 17:51:46 +0300

    відступи

--- src/pages/Layout/LayoutStyled.js
+++ src/pages/Layout/LayoutStyled.js
@@ -9,16 +9,16 @@ export const Wrapper = styled.div`
 export const Main = styled.main`
   flex: 1;
   display: flex;
-  margin-top: 40px;
-  margin-bottom: 32px;
+  /* margin-top: 40px;
+  margin-bottom: 32px; */
 
   @media screen and (min-width: 768px) {
-    margin-top: 64px;
-    margin-bottom: 48px;
+    /* margin-top: 64px;
+    margin-bottom: 48px; */
   }
 
   @media screen and (min-width: 1280px) {
-    margin-top: 72px;
-    margin-bottom: 56px;
+    /* margin-top: 72px;
+    margin-bottom: 56px; */
   }
 `;
